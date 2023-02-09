00e501534bccacbf7f61e0b7d8661869b330b645
Uliana Koshuba <koshubaulyana@gmail.com>
2023-02-09 17:51:09 +0200

    c

--- lifecycle-methods/src/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Demo from './Demo';
-import './index.scss';
-// import Life from './Life';
-
-const rootElement = document.querySelector('#root');
-
-ReactDOM.render(<Demo />, rootElement);
