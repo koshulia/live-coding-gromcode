425bbd3637f6c33f9c4283efc23c362159408c67
Uliana Koshuba <koshubaulyana@gmail.com>
2023-02-09 17:44:51 +0200

    c

--- lifecycle-methods/src/Life.jsx
+++ lifecycle-methods/src/Life.jsx
@@ -10,10 +10,8 @@ class Life extends Component {
     console.log('componentDidMount: API calls, subscriptions');
   }
 
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log(nextProps, nextState);
+  shouldComponentUpdate() {
     console.log('shouldComponentUpdate(nextProps, nextState): decide to render or not to render');
-    return nextProps.number;
   }
 
   componentDidUpdate() {
