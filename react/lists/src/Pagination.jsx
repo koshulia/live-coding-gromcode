9e902a92209291da2324d8ab5b79c472e06e6abc
Uliana Koshuba <koshubaulyana@gmail.com>
2023-02-22 17:22:57 +0200

    update

--- react/lists/src/Pagination.jsx
+++ react/lists/src/Pagination.jsx
@@ -8,13 +8,13 @@ import React from 'react';
 // usersPerPage
 
 const Pagination = props => {
-  const isPrevPageAvailable = props.currentPage === 1;
+  const isPrevPageAvailable = props.currentPage > 1;
   const isNextPageAvailable = props.totalItems.length - props.currentPage * props.itemsPerPage > 0;
 
   return (
     <div className="pagination">
-      <button className="btn" onClick={props.goPrev} disabled={isPrevPageAvailable}>
-        {!isPrevPageAvailable && '←'}
+      <button className="btn" onClick={props.goPrev} disabled={!isPrevPageAvailable}>
+        {isPrevPageAvailable && '←'}
       </button>
       <span className="pagination__page">{props.currentPage}</span>
       <button className="btn" onClick={props.goNext} disabled={!isNextPageAvailable}>
